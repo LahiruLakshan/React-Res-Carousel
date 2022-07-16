f1bc392b3e6bac7a81872fd212957028fa21fdde
LahiruLakshan <balahirulakshan@gmail.com>
2022-07-17 03:18:43 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+import ReactPlayer from 'react-player';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 function App() {
+
+    function handleDuration(duration) {
+        console.log('onDuration', duration)
+    }
+
+    const YoutubeSlide = ({ url, isSelected }) => (
+      <ReactPlayer width="100%" height={"100%"} url={url} playing={true} loop muted controls onDuration={(e)=>handleDuration(e)}/>
+  );
+
+  const customRenderItem = (item, props) => <item.type {...item.props} {...props} />;
+
+  const getVideoThumb = (videoId) => `https://img.youtube.com/vi/${videoId}/default.jpg`;
+
+  const getVideoId = (url) => url.substr('https://www.youtube.com/embed/'.length, url.length);
+
+    const customRenderThumb = (children) =>
+        children.map((item) => {
+            const videoId = getVideoId(item.props.url);
+
+            return <img src={getVideoThumb(videoId)} />;
+        });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Carousel
+           autoPlay={true} interval={15000} stopOnHover={false}
+          infiniteLoop={true} renderItem={customRenderItem} renderThumbs={customRenderThumb}>
+        <YoutubeSlide key="youtube-1" url="https://firebasestorage.googleapis.com/v0/b/display-client.appspot.com/o/10%20Second%20Video_%20Stuffed%20Grilled%20Sandwiches.mp4a0d91532-aeaa-4b74-9c9c-96d87757d6e7?alt=media&token=bf269397-6d22-4e93-bfb8-796eedaad3ab"/>
+        <YoutubeSlide key="youtube-2" url="https://www.youtube.com/embed/mOdmi9SVeWY" />
+          {/*<img src={"https://img.youtube.com/vi/n0F6hSpxaFc/default.jpg"} alt={"sdf"}/>*/}
+        <YoutubeSlide key="youtube-3" url="https://www.youtube.com/embed/n0F6hSpxaFc" />
+        <YoutubeSlide key="youtube-4" url="https://www.youtube.com/embed/0uGETVnkujA" />
+      </Carousel>
   );
 }
 
